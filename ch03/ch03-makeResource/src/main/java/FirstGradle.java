9cf8dfad4b6ce5646f4d556dea309a695a5b72e8
grayjin <darkandark90@gmail.com>
2015-08-04 22:08:08 +0900

    folder change make resource

---
+++ ch03/ch03-makeResource/src/main/java/FirstGradle.java
@@ -0,0 +1,9 @@
+public class FirstGradle{
+
+  public FirstGradle(){}
+
+  public static void main(String ar[]){
+    FirstGradle gradle = new FirstGradle();
+    System.out.println("Hello Gradle with Java");
+  }  
+}
